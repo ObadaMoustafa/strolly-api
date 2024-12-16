61ddd7cc39879a57f1ce968f09bad51490bbb17b
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2024-12-16 10:43:18 +0100

    commented out robs code

--- src/index.js
+++ src/index.js
@@ -32,22 +32,22 @@ const startServer = async () => {
  * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
  * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
  */
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(new URL("../../client/dist", import.meta.url).pathname)
-  );
-  // Redirect * requests to give the client data
-  app.get("*", (req, res) =>
-    res.sendFile(
-      new URL("../../client/dist/index.html", import.meta.url).pathname
-    )
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(
+//     express.static(new URL("../../client/dist", import.meta.url).pathname)
+//   );
+//   // Redirect * requests to give the client data
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       new URL("../../client/dist/index.html", import.meta.url).pathname
+//     )
+//   );
+// }
 
-/****** For cypress we want to provide an endpoint to seed our data ******/
-if (process.env.NODE_ENV !== "production") {
-  app.use("/api/test", testRouter);
-}
+// /****** For cypress we want to provide an endpoint to seed our data ******/
+// if (process.env.NODE_ENV !== "production") {
+//   app.use("/api/test", testRouter);
+// }
 
 // Start the server
 startServer();
